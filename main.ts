5e8b1a2c0b09e2591ab809c21c1bdc958658d35c
yasu1 <97661658+yasu159@users.noreply.github.com>
2022-08-13 13:48:15 +0900

    spagetthi~

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, Workspace, WorkspaceSidedock } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, Workspace, WorkspaceRibbon, WorkspaceSidedock } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
@@ -12,16 +12,17 @@ const DEFAULT_SETTINGS: AutoHideSettings = {
 
 export default class AutoHidePlugin extends Plugin {
 	settings: AutoHideSettings;
-	sidedock: WorkspaceSidedock;
-	workspace: Workspace;
-	leftRibbon: HTMLElement;
-	rightRibbon: HTMLElement;
-	leftSidebar: HTMLElement;
-	rightSidebar: HTMLElement;
-	
+	// sidedock: WorkspaceSidedock;
+	// workspace: Workspace;
+	leftSplit: WorkspaceSidedock;
+	rightSplit: WorkspaceSidedock;
+	leftRibbon: WorkspaceRibbon;
+	rightRibbon: WorkspaceRibbon;
+
+
 	async onload() {
 		await this.loadSettings();
-		
+
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
@@ -29,28 +30,7 @@ export default class AutoHidePlugin extends Plugin {
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
-		
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		// const statusBarItemEl = this.addStatusBarItem();
-		// statusBarItemEl.setText('Status Bar Text');
-		
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
+
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
 			id: 'open-sample-modal-complex',
@@ -64,23 +44,26 @@ export default class AutoHidePlugin extends Plugin {
 					if (!checking) {
 						new SampleModal(this.app).open();
 					}
-					
+
 					// This command will only show up in Command Palette when the check function returns true
 					return true;
 				}
 			}
 		});
-		
+
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-		
+
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		// const LB = this.app.workspace.leftRibbon.containerEl;
-		this.registerDomEvent(this.app.workspace.leftRibbon.containerEl, 'mouseover', (evt: MouseEvent) => {
-			console.log('click', evt);
+		console.log("outer ready");
+		this.app.workspace.onLayoutReady(() => {
+			this.initialize();
+			this.registerEvents()
 			
-		});
+		})
+
+		
 		
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
@@ -97,6 +80,37 @@ export default class AutoHidePlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	initialize() {
+		this.leftSplit = this.app.workspace.leftSplit;
+		this.rightSplit = this.app.workspace.rightSplit;
+		this.leftRibbon = this.app.workspace.leftRibbon;
+		this.rightRibbon = this.app.workspace.rightRibbon;
+		console.log("Initialize done");
+
+	}
+
+	registerEvents() {
+		// this.registerDomEvent(this.leftRibbon.containerEl, 'mouseover', (evt: MouseEvent) => {
+		// 	console.log('mouseover', evt);
+		// 	this.app.workspace.leftSplit.expand();
+		// });
+		// this.registerDomEvent(this.rightRibbon.containerEl, 'mouseover', (evt: MouseEvent) => {
+		// 	console.log('mouseover', evt);
+		// });
+
+		this.registerDomEvent(this.app.workspace.rootSplit.containerEl, 'click', (evt: MouseEvent) => {
+			console.log('click', evt);
+			this.collapseAll();			
+		});
+	}
+
+	collapseAll() {
+		this.leftSplit.collapse();
+		this.rightSplit.collapse();
+	}
+
+
 }
 
 class SampleModal extends Modal {
