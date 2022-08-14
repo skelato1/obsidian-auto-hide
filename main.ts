933d36863db4bca1f7109ae15693df71dba9e5f5
yasu1 <97661658+yasu159@users.noreply.github.com>
2022-08-14 15:50:30 +0900

    Added expand sidebar options.

--- main.ts
+++ main.ts
@@ -1,76 +1,40 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, Workspace, WorkspaceRibbon, WorkspaceSidedock } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, Workspace, WorkspaceRibbon, WorkspaceRoot, WorkspaceSidedock } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
 interface AutoHideSettings {
-	mySetting: string;
+	openLeftSplitOnClickSideBar: boolean;
+	openLeftSplitOnClickTitleBar: boolean;
 }
 
 const DEFAULT_SETTINGS: AutoHideSettings = {
-	mySetting: 'default'
+	openLeftSplitOnClickSideBar: false,
+	openLeftSplitOnClickTitleBar: false
 }
 
 export default class AutoHidePlugin extends Plugin {
 	settings: AutoHideSettings;
-	// sidedock: WorkspaceSidedock;
-	// workspace: Workspace;
 	leftSplit: WorkspaceSidedock;
 	rightSplit: WorkspaceSidedock;
-	leftRibbon: WorkspaceRibbon;
-	rightRibbon: WorkspaceRibbon;
-
+	leftRibbonEl: HTMLElement;
+	rightRibbonEl: HTMLElement;
+	contentEl: HTMLElement;
+	titleBarEl: HTMLElement;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		console.log("outer ready");
 		this.app.workspace.onLayoutReady(() => {
-			this.initialize();
-			this.registerEvents()
-			
+			this.init();
+			this.registerEvents();
 		})
-
-		
-		
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 	
 	onunload() {
-		
+
 	}
 	
 	async loadSettings() {
@@ -81,51 +45,45 @@ export default class AutoHidePlugin extends Plugin {
 		await this.saveData(this.settings);
 	}
 
-	initialize() {
+	init() {
 		this.leftSplit = this.app.workspace.leftSplit;
 		this.rightSplit = this.app.workspace.rightSplit;
-		this.leftRibbon = this.app.workspace.leftRibbon;
-		this.rightRibbon = this.app.workspace.rightRibbon;
-		console.log("Initialize done");
-
+		this.leftRibbonEl = this.app.workspace.leftRibbon.containerEl;
+		this.rightRibbonEl = this.app.workspace.rightRibbon.containerEl;
+		this.contentEl = document.getElementsByClassName('view-content')[0] as HTMLElement;
+		this.titleBarEl = document.getElementsByClassName('view-header-title-container')[0] as HTMLElement;
 	}
 
 	registerEvents() {
-		// this.registerDomEvent(this.leftRibbon.containerEl, 'mouseover', (evt: MouseEvent) => {
-		// 	console.log('mouseover', evt);
-		// 	this.app.workspace.leftSplit.expand();
-		// });
-		// this.registerDomEvent(this.rightRibbon.containerEl, 'mouseover', (evt: MouseEvent) => {
-		// 	console.log('mouseover', evt);
-		// });
-
-		this.registerDomEvent(this.app.workspace.rootSplit.containerEl, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-			this.collapseAll();			
+		// Click rootSplit to collapse all.
+		this.registerDomEvent(this.contentEl, 'click', (evt: MouseEvent) => {
+			this.leftSplit.collapse();
+			this.rightSplit.collapse();		
 		});
-	}
 
-	collapseAll() {
-		this.leftSplit.collapse();
-		this.rightSplit.collapse();
-	}
+		// Click leftRibbon to expand leftSplit (Optional).
+		this.registerDomEvent(this.leftRibbonEl, 'click', (evt: MouseEvent) => {
+			if(this.settings.openLeftSplitOnClickSideBar){
+				if(evt.target.ariaLabel == null){ // =サイドバー内のボタンを押している場合は実行しない
+					this.leftSplit.expand();
+				}
+			}
+		});
+		// Click rightRibbon to expand rightSplit (Optional).
+		this.registerDomEvent(this.rightRibbonEl, 'click', (evt: MouseEvent) => {
+			if(this.settings.openLeftSplitOnClickSideBar){
+				if(evt.target.ariaLabel == null){ // =サイドバー内のボタンを押している場合は実行しない
+					this.rightSplit.expand();
+				}
+			}
+		});
 
-
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+		// Click titleBar to expand leftSplit (Optional).
+		this.registerDomEvent(this.titleBarEl, 'click', (evt: MouseEvent) => {
+			if(this.settings.openLeftSplitOnClickTitleBar){
+				this.leftSplit.expand();
+			}
+		});
 	}
 }
 
@@ -142,17 +100,26 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Settings for Auto Hide plugin.'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('Click sidebar to expand')
+			.setDesc('When you clicked the left ribbon bar, then left sidebar expand')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.openLeftSplitOnClickSideBar)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					console.log('asyncyncyn!');
+					this.plugin.settings.openLeftSplitOnClickSideBar = value;
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('Click titlebar to expand sidebar')
+			.setDesc('When you clicked the left ribbon bar, then left sidebar expand')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.openLeftSplitOnClickTitleBar)
+				.onChange(async (value) => {
+					console.log('asyncyncyn!');
+					this.plugin.settings.openLeftSplitOnClickTitleBar = value;
 					await this.plugin.saveSettings();
 				}));
 	}
