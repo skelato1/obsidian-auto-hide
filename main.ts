e7aa0c0748ad11049f87e0d526120a95bb153ecc
skelato1 <97661658+skelato1@users.noreply.github.com>
2022-10-03 09:52:20 +0900

    added lock button#2 (without css)

--- main.ts
+++ main.ts
@@ -1,13 +1,15 @@
-import { App, Plugin, PluginSettingTab, Setting, WorkspaceSidedock } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting, WorkspaceSidedock, ButtonComponent } from 'obsidian';
 
 interface AutoHideSettings {
 	expandSidebar_onClickRibbon: boolean;
 	expandSidebar_onClickNoteTitle: boolean;
+	lockSidebar: boolean;
 }
 
 const DEFAULT_SETTINGS: AutoHideSettings = {
 	expandSidebar_onClickRibbon: false,
-	expandSidebar_onClickNoteTitle: false
+	expandSidebar_onClickNoteTitle: false,
+	lockSidebar: false
 }
 
 export default class AutoHidePlugin extends Plugin {
@@ -18,6 +20,9 @@ export default class AutoHidePlugin extends Plugin {
 	leftRibbonEl: HTMLElement;
 	rightRibbonEl: HTMLElement;
 
+	leftPin: boolean;
+	rightPin: boolean;
+
 	async onload() {
 		await this.loadSettings();
 
@@ -26,11 +31,12 @@ export default class AutoHidePlugin extends Plugin {
 		this.app.workspace.onLayoutReady(() => {
 			this.init();
 			this.registerEvents();
+			this.togglePins();
 		})
 	}
 
 	onunload() {
-
+		this.removePins();
 	}
 
 	async loadSettings() {
@@ -58,8 +64,8 @@ export default class AutoHidePlugin extends Plugin {
 				}
 			} else { // Click on the rootSplit() to collapse both sidebars.
 				if (!(evt.target.classList.contains("cm-hashtag") || evt.target.classList.contains("tag"))) {
-					this.leftSplit.collapse();
-					this.rightSplit.collapse();
+					if (!this.leftPin) this.leftSplit.collapse();
+					if (!this.rightPin) this.rightSplit.collapse();
 				}
 			}
 		});
@@ -82,7 +88,59 @@ export default class AutoHidePlugin extends Plugin {
 			}
 		});
 	}
+
+	// Feature: pane locking
+
+	togglePins() {
+		if (this.settings.lockSidebar) {
+			this.addPins();
+		} else {
+			this.removePins();
+		}
+	}
+
+	addPins() {
+		const tabHeaderContainers = this.app.workspace.containerEl.getElementsByClassName("workspace-tab-header-container");
+		this.leftPin = false;
+		this.rightPin = false;
+
+		const lb = new ButtonComponent(tabHeaderContainers[0] as HTMLElement)
+			.setIcon("pin")
+			.setClass("auto-hide-button")
+			.onClick((evt: any) => {
+				this.leftPin = !this.leftPin;
+				if (this.leftPin) {
+					lb.setIcon("filled-pin");
+				} else {
+					lb.setIcon("pin");
+				}
+			});
+
+		const rb = new ButtonComponent(tabHeaderContainers[1] as HTMLElement)
+			.setIcon("pin")
+			.setClass("auto-hide-button")
+			.onClick((evt: any) => {
+				this.rightPin = !this.rightPin;
+				if (this.rightPin) {
+					rb.setIcon("filled-pin");
+				} else {
+					rb.setIcon("pin");
+				}
+			});
+	}
+
+	removePins() {
+		const pins = document.getElementsByClassName("auto-hide-button");
+		while (pins.length) {
+			console.log(pins.item(0), "removing!")
+			if (pins.item(0) != null) {
+				pins[0].remove();
+			}
+		}
+
+	}
 }
+
 
 class AutoHideSettingTab extends PluginSettingTab {
 	plugin: AutoHidePlugin;
@@ -116,6 +174,16 @@ class AutoHideSettingTab extends PluginSettingTab {
 				.onChange(async (value) => {
 					this.plugin.settings.expandSidebar_onClickNoteTitle = value;
 					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('Lock sidebar collapse')
+			.setDesc('Add a pin that can temporarily lock the sidebar collapse.')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.lockSidebar)
+				.onChange(async (value) => {
+					this.plugin.settings.lockSidebar = value;
+					await this.plugin.saveSettings();
+					this.plugin.togglePins();
 				}));
 	}
 }
